43f6a096485dff8cf59a4fac0e99d09dd6749f5e
cheiru94 <cheiru94@gmail.com>
2023-10-04 18:43:14 +0900

    clear

--- programmers/Lv. 0/test9.js
+++ programmers/Lv. 0/test9.js
@@ -2,9 +2,11 @@
 
 function solution(arr) {
     var answer = '';
+
+    // 문자열을 하나씩 따개자
     for (let i = 0; i < arr.length; i++) {
-        answer += arr[i]
-        console.log(answer)
+        answer += arr[i]  // 문자열 하나씩 대입
+        console.log(answer) // 확인용
     }
     return answer;
 }
